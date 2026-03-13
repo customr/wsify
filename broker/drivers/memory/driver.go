2c03f1e7b3a5e9a7b96b8c829e9fefbe45df2291
customr <shipicin_max@mail.ru>
2026-03-13 21:01:22 +0400

    fix: ws

--- broker/drivers/memory/driver.go
+++ broker/drivers/memory/driver.go
@@ -2,8 +2,9 @@ package memorybroker
 
 import (
 	"context"
-	"github.com/savsgio/gotils/uuid"
 	"sync"
+
+	"github.com/savsgio/gotils/uuid"
 )
 
 type Driver struct {
@@ -18,17 +19,17 @@ type Driver struct {
 func (d *Driver) Connect(_ string) error {
 	d.Lock()
 	defer d.Unlock()
-	
+
 	d.subscriptions = make(map[string]map[string]chan []byte)
 	d.doneChannels = make(map[string]chan struct{})
 	d.closed = make(chan struct{})
-	
+
 	return nil
 }
 
 func (d *Driver) Subscribe(ctx context.Context, channel string) (<-chan []byte, chan struct{}, error) {
 	d.Lock()
-	
+
 	id := uuid.V4()
 	messagesChan := make(chan []byte, 100)
 	doneChan := make(chan struct{}, 1)
@@ -44,20 +45,25 @@ func (d *Driver) Subscribe(ctx context.Context, channel string) (<-chan []byte, 
 	d.wg.Add(1)
 	go func() {
 		defer d.wg.Done()
-		
+
 		select {
 		case <-doneChan:
 		case <-ctx.Done():
 		case <-d.closed:
 		}
-		
+
 		d.Lock()
 		defer d.Unlock()
-		
+
+		// SAFELY remove subscription
 		if ch, exists := d.subscriptions[channel][id]; exists {
+			// Close the channel (receivers will see it's closed)
 			close(ch)
+
+			// Remove from subscriptions map
 			delete(d.subscriptions[channel], id)
-			
+
+			// Clean up empty channel map
 			if len(d.subscriptions[channel]) == 0 {
 				delete(d.subscriptions, channel)
 			}
@@ -77,11 +83,21 @@ func (d *Driver) Publish(_ context.Context, channel string, msg []byte) error {
 	d.RUnlock()
 
 	for _, subscriber := range subscribers {
-		select {
-		case subscriber <- msg:
-		default:
-			// Channel full, skip to prevent blocking
-		}
+		// SAFE SEND: Use select with recover to catch closed channel panics
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					// Channel was closed, just ignore
+				}
+			}()
+
+			select {
+			case subscriber <- msg:
+				// Success
+			default:
+				// Channel full, skip to prevent blocking
+			}
+		}()
 	}
 
 	return nil
@@ -90,8 +106,9 @@ func (d *Driver) Publish(_ context.Context, channel string, msg []byte) error {
 func (d *Driver) Close() error {
 	d.closeOnce.Do(func() {
 		close(d.closed)
-		
+
 		d.Lock()
+		// Signal all done channels
 		for id, ch := range d.doneChannels {
 			select {
 			case ch <- struct{}{}:
@@ -101,19 +118,25 @@ func (d *Driver) Close() error {
 			delete(d.doneChannels, id)
 		}
 		d.Unlock()
-		
+
+		// Wait for all cleanup goroutines
 		d.wg.Wait()
-		
+
 		d.Lock()
+		// Now safe to close all subscription channels
 		for channel, subs := range d.subscriptions {
 			for id, ch := range subs {
-				close(ch)
+				// Safe close with recover
+				func() {
+					defer func() { recover() }()
+					close(ch)
+				}()
 				delete(subs, id)
 			}
 			delete(d.subscriptions, channel)
 		}
 		d.Unlock()
 	})
-	
+
 	return nil
 }
