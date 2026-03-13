f7bd7aa8f40965323e73a86650e9afcd0cc33e4f
customr <shipicin_max@mail.ru>
2026-03-13 10:26:50 +0400

    fix: ws

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	srv.Use(middleware.Recover())
 	srv.Use(middleware.CORS())
-	srv.Use(middleware.Logger())
+	// srv.Use(middleware.Logger())
 
 	srv.GET("/ws", func(c echo.Context) error {
 		atomic.AddInt64(&activeConnections, 1)
